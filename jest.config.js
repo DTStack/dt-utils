931244cb458134d9e0aa843c06a1ceb29fb87686
linhe <linhe@dtstack.com>
2021-01-20 17:14:17 +0800

    feat(test): add test for utils

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    globals: {
+        'ts-jest':{
+            isolatedModules: true, 
+        },
+    },
+    testPathIgnorePatterns: ['/node_modules/'],
+    testMatch: [
+        '**/__tests__/**/(*.)+(spec|test).[jt]s?(x)',
+        '**/test/**/(*.)+(spec|test).[jt]s?(x)',
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,8 +1,84 @@
 import assert from 'assert';
 import Utils from '../src/utils';
-const { checkExist,getCssText,trim } = Utils;
-
+const { convertBytes, checkExist, getCssText, trim, trimlr, isMacOs, isWindows, isMobileDevice, getParameterByName, percent, removeAllSpaces, toQfw, textOverflowExchange, exchangeOrder, isEqualArr } = Utils;
+describe('utils.convertBytes', () => {
+    test('convert byte to unit B', () => {
+        const byte = 10.24;
+        const expected = '10.24 B';
+        const newVal = convertBytes(byte);
+        expect(newVal).toEqual(expected);
+    });
+    test('convert byte to unit KB', () => {
+        const byte = 1024;
+        const expected = '1 KB';
+        const newVal = convertBytes(byte);
+        expect(newVal).toEqual(expected);
+    });
+    test('convert byte to unit MB', () => {
+        const byte = 10241024;
+        const expected = '9.77 MB';
+        const newVal = convertBytes(byte);
+        expect(newVal).toEqual(expected);
+    });
+    test('convert byte to unit GB', () => {
+        const byte = 3029021814;
+        const expected = '2.82 GB';
+        const newVal = convertBytes(byte);
+        expect(newVal).toEqual(expected);
+    });
+    test('convert byte to unit TB', () => {
+        const byte = 1184380146909; // 1024 * 5;
+        const expected = '1.08 TB';
+        const newVal = convertBytes(byte);
+        expect(newVal).toEqual(expected);
+    });
+    test('convert byte to unit PB', () => {
+        const byte = 1125899906842624;
+        const expected = '1 PB';
+        const newVal = convertBytes(byte);
+        expect(newVal).toEqual(expected);
+    });
+});
 describe('utils:', () => {
+    test('isMacOs=>false', () => {
+        assert.strictEqual(isMacOs(), false);
+    });
+    test('isWindows=>false', () => {
+        assert.strictEqual(isWindows(), false);
+    });
+    test('isMobileDevice=>false', () => {
+        assert.strictEqual(isMobileDevice(), false);
+    });
+    test('getParameterByName()=>张三', () => {
+        assert.strictEqual(getParameterByName('name', 'http://gitlab.prod.dtstack.cn?name=张三'), '张三');
+    });
+    test('percent(1)=>100%', () => {
+        assert.strictEqual(percent(1), '100%');
+    });
+    test('percent(0.5)=>50%', () => {
+        assert.strictEqual(percent(0.5), '50%');
+    });
+    test('percent(0.54321,1)=>50%', () => {
+        assert.strictEqual(percent(0.54321, 1), '54.3%');
+    });
+    test('removeAllSpaces(\' aa bb \')=>aabb', () => {
+        assert.strictEqual(removeAllSpaces(' aa bb '), 'aabb');
+    });
+    test('toQfw(\'123123\')=>123,123', () => {
+        assert.strictEqual(toQfw('123123'), '123,123');
+    });
+    test('textOverflowExchange(\'my name is linhe\')=>"my name is..."', () => {
+        assert.strictEqual(textOverflowExchange('my name is linhe', 10), 'my name is...');
+    });
+    test('exchangeOrder(\'ascend\')=>asc', () => {
+        assert.strictEqual(exchangeOrder('ascend'), 'asc');
+    });
+    test('exchangeOrder(\'descend\')=>desc', () => {
+        assert.strictEqual(exchangeOrder('descend'), 'desc');
+    });
+    test('isEqualArr([1,2,3],[1,2,3])=>true', () => {
+        assert.strictEqual(isEqualArr(['1', '2', '3'], ['1', '2', '3']), true);
+    });
     /**
    * checkExist
    */
@@ -21,16 +97,16 @@ describe('utils:', () => {
         });
     });
     /**
-   * getCssText
-   */
+    * getCssText
+    */
     describe('getCssText', () => {
         test(' {height:\'100px\',width:\'100px\'} => height:100px;width:100px; ', () => {
-            assert.strictEqual(getCssText({height:'100px',width:'100px'}), 'height:100px;width:100px;');
+            assert.strictEqual(getCssText({ height: '100px', width: '100px' }), 'height:100px;width:100px;');
         });
     });
     /**
-   * trim
-   */
+        * trim
+    */
     describe('trim', () => {
         test('\' 张三 \'=> 张三 ', () => {
             assert.strictEqual(trim(' 张三 '), '张三');
@@ -44,5 +120,9 @@ describe('utils:', () => {
         test('\'张 三\'=> 张三', () => {
             assert.strictEqual(trim('张 三'), '张 三');
         });
+        test('\'张 三\'=> 张 三', () => {
+            assert.strictEqual(trimlr('张 三'), '张 三');
+        });
     });
 });
+
