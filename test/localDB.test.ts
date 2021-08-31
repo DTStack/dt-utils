24dd3f4242c6e286709c1af0f714cdd116bfdbf3
“Zaoei” <jingming@dtstack.com>
2021-08-31 20:22:57 +0800

    refactor: optimize get method, add unit testing

---
+++ test/localDB.test.ts
@@ -0,0 +1,54 @@
+// storage.test.js
+
+import LocalDB from '../src/localDB';
+
+const fakeLocalStorage = (function () {
+    let store = {};
+
+    return {
+        store,
+        getItem: function (key) {
+            return this[key] || null;
+        },
+        setItem: function (key, value) {
+            this[key] = value.toString();
+        },
+        removeItem: function (key) {
+            delete this[key];
+        },
+        clear: function () {
+            store = {};
+        },
+    };
+})();
+
+describe('utils.LocalDB', () => {
+    beforeAll(() => {
+        const localSProxy = new Proxy(fakeLocalStorage, {
+            get: function (target, property) {
+                if (target.store[property]) {
+                    return target.store[property];
+                }
+                return target[property];
+            },
+        });
+        Object.defineProperty(window, 'localStorage', {
+            value: localSProxy,
+        });
+    });
+
+    test('get the key to the string storage', () => {
+        window.localStorage.setItem('loglevel:webpack-dev-server', 'SILENT');
+        const value = LocalDB.get('loglevel:webpack-dev-server');
+        const expected = 'SILENT';
+        expect(value).toEqual(expected);
+    });
+    test('get the key to the json string storage', () => {
+        window.localStorage.setItem('jsonStr', '{"string":1}');
+        const value = LocalDB.get('jsonStr');
+        const expected = {
+            string: 1,
+        };
+        expect(value).toEqual(expected);
+    });
+});
