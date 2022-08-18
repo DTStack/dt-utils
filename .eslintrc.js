424506895b07e44af9c422a0801d253f13606a59
liuyi <>
2022-08-18 21:34:01 +0800

    ci: optimize ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: [require.resolve('ko-lint-config/.eslintrc')],
     rules: {
-      'no-console': 0,
-      'no-param-reassign': 0, // 函数的入参再次赋值
+        'no-console': 0,
+        'no-param-reassign': 0, // 函数的入参再次赋值
     },
 };
