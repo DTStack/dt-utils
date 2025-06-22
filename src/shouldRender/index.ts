3166fec327f514f24ed832a6c6b50044eb6b14d8
jin-sir <942725119@qq.com>
2025-06-23 01:04:38 +0800

    feat: add shouldRender

---
+++ src/shouldRender/index.ts
@@ -0,0 +1,29 @@
+import { isEqual } from 'lodash-es';
+
+/**
+ * A class decorator that automatically implements shouldComponentUpdate for React components
+ * by performing deep equality checks on props and state.
+ *
+ * @deprecated This decorator is deprecated. Consider using React.memo() or React.PureComponent instead.
+ *
+ * @param {any} target - The target class to be decorated
+ *
+ * @example
+ * ```typescript
+ * import { shouldRender } from 'dt-utils';
+ *
+ * @shouldRender
+ * class MyComponent extends React.Component {
+ *   render() {
+ *     return <div>{this.props.data}</div>;
+ *   }
+ * }
+ * ```
+ */
+const shouldRender = (target: any) => {
+    target.prototype.shouldComponentUpdate = function (nextProps: any, nextState: any) {
+        return !isEqual(this.state, nextState) || !isEqual(this.props, nextProps);
+    };
+};
+
+export default shouldRender;
